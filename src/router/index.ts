0122249bf6b6d8d2d930d7626562b70dcb5d822b
abdallahbahrawi1 <abdallah.bahrawi400@gmail.com>
2024-03-19 18:59:06 +0200

    finish the navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import AboutView from '../views/AboutView.vue'
 import HomeView from '../views/HomeView.vue'
+import WorkView from '../views/WorkView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
+  }
+  ,
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/work',
+    name: 'work',
+    component: WorkView
   }
 ]
 
